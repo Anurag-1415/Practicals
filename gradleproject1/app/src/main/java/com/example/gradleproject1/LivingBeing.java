8bb8ea0f6a8159d20b34b2fd8dd32f927da75b52
Anurag-1415 <2024bit505@sggs.ac.in>
2025-03-28 09:38:34 +0530

    TASK

---
+++ gradleproject1/app/src/main/java/com/example/gradleproject1/LivingBeing.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.gradleproject1;
+
+/**
+ *
+ * @author andy
+ */
+public class LivingBeing {
+    
+    private byte age;
+    private String type;
+    
+    public int getAge() {
+        return age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LivingBeing(byte age, String type) {
+        this.age = age;
+        this.type = type;
+    }
+
+}
